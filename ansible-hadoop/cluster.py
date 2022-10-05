216a89c8f79c4f2a57efc383e8a0a09c1b87b1bc
gyojun <sencom1028@gmail.com>
2022-10-06 01:36:10 +0900

    Add cluster init script

--- ansible-hadoop/cluster.py
+++ ansible-hadoop/cluster.py
@@ -1,72 +1,40 @@
-import sys, os
+import os
 from collections import defaultdict, deque
 
 HOME_PATH='/ansible/ansible-hadoop'
-
-COMPONENT = {
-    'airflow': True,
-    'python': False,
-    'mysql': True,
-    'hadoop': True,
-    'superset': True,
-    'hive': True,
-    'scala': False,
-    'java': False,
-    'kafka': True,
-    'zookeeper': True,
-    'spark': True,
-    'tez': False
+END = []
+TOPOLOGY = {
+    'zookeeper': ['hadoop'],
+    'mysql': ['hadoop'],
+    # hadoop-setup -> tez-setup, spark-setup -> hive-setup
+    'hadoop': END,
+    'airflow': END,
 }
 
-STACK = {
-    'airflow': ['python', 'mysql'],
-    'hadoop': ['java', 'zookeeper'],
-    'mysql': [],
-    'superset': ['python', 'mysql'],
-    'hive': ['hadoop', 'mysql', 'zookeeper', 'tez'],
-    'kafka': ['java', 'zookeeper'],
-    'zookeeper': ['java'],
-    'spark': ['scala', 'hive', 'hadoop'],
-    'hue': ['hive', 'hadoop']
-}
 
-def gen_topology_indegree():
-    indegree = defaultdict(int)
-    for stack, childs in STACK.items():
-        if indegree[stack] == 0:
-            indegree[stack] = 0
-        for child in childs:
-            indegree[child] += 1
-    return indegree
-
-
-def ansible_command(act, target):
-    return f'ansible-playbook -i {HOME_PATH}/inventories/host.yml {HOME_PATH}/{target}-{act}.yml'
-
-
-def setup_cluster(indegree):
-    q = deque()
-    for target, v in indegree.items():
-        if v == 0:
-            q.append(target)
-    while q:
-        target = q.popleft()
-        if COMPONENT[target]:
-            print(os.system(ansible_command('setup', target)))
-        for child in STACK[target]:
-            indegree[child] -= 1
-            if indegree[child] == 0:
-                q.append(child)
+def play(target, act='setup', use_extra_vars=False):
+    ansible_command = None
+    if not use_extra_vars:
+        ansible_command = f'ansible-playbook -i {HOME_PATH}/inventories/host.yml {HOME_PATH}/{target}-{act}.yml'
+    else:
+        ansible_command = f'ansible-playbook -i {HOME_PATH}/inventories/host.yml {HOME_PATH}/{target}.yml --extra-vars \"act={act}\"'
+    print(os.system(ansible_command))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('need one argument: setup | start | stop')
-        sys.exit(2)
-    
+    # init hadoop cluster
+    indegree = defaultdict(int)
+    for parent, childs in TOPOLOGY.items():
+        if parent not in indegree:
+            indegree[parent] = 0
+        for child in childs:
+            indegree[child] += 1
 
-    print(os.system(build_ansible_command()))
-    
-
-    gen_topology_indegree
-    pass
+    q = deque([k for k, v in indegree.items() if v == 0])
+    while q:
+        parent = q.popleft()
+        play(parent)
+        for child in TOPOLOGY[parent]:
+            indegree[child] -= 1
+            if indegree[child] == 0:
+                q.append(child)
